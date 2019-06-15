9b9ff193a8a755a2fe9d4ad00f8caa485db2f749
thisSyntaxSucksAndYouKnowIt <45504882+thisSyntaxSucksAndYouKnowIt@users.noreply.github.com>
2019-06-15 22:31:21 +0200

    .

--- automate_void.sh
+++ automate_void.sh
@@ -17,46 +17,46 @@ mv .Xresources /home/$(whoami)
 sudo mv xinitrc /etc/X11/xinit
 
 echo "Updating system"
-y | sudo xbps-install -Su
+yes | sudo xbps-install -Su
 
 echo "Installing xorg"
-y | sudo xbps-install -S xorg
+yes | sudo xbps-install -S xorg
 
 echo "Installing alsa-utils"
-y | sudo xbps-install -S alsa-utils
+yes | sudo xbps-install -S alsa-utils
 
 echo "Installing rxvt-unicode"
-y | sudo xbps-install -S rxvt-unicode
+yes | sudo xbps-install -S rxvt-unicode
 
 echo "Installing i3 gaps"
-y | sudo xbps-install -S i3-gaps
+yes | sudo xbps-install -S i3-gaps
 
 echo "Installing i3 status"
-y | sudo xbps-install -S i3status
+yes | sudo xbps-install -S i3status
 
 echo "Installing dmenu"
-y | sudo xbps-install -S dmenu
+yes | sudo xbps-install -S dmenu
 
 echo "Installing i3 feh"
-y | sudo xbps-install -S feh
+yes | sudo xbps-install -S feh
 
 echo "Installing dev tools"
-y | sudo xbps-install -S base-devel
+yes | sudo xbps-install -S base-devel
 
 echo "Installing wget"
-y | sudo xbps-install -S wget
+yes | sudo xbps-install -S wget
 
 echo "Installing Python3"
-y | sudo xbps-install -S python3
+yes | sudo xbps-install -S python3
 
 echo "Installing Pip3"
-y | sudo xbps-install -S python3-pip
+yes | sudo xbps-install -S python3-pip
 
 echo "Installing Radare2"
-y | sudo xbps-install -S radare2
+yes | sudo xbps-install -S radare2
 
 echo "Installing Vim"
-y | sudo xbps-install -S vim
+yes | sudo xbps-install -S vim
 
 echo "Installing Vim plugins"
 cd/home/$(whoami)
@@ -65,7 +65,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 echo "Installing Git"
-y | sudo xbps-install -S git
+yes | sudo xbps-install -S git
 
 echo "Installing Neofetch"
-y | sudo xbps-install -S neofetch
+yes | sudo xbps-install -S neofetch
